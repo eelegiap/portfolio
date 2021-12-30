67ba63ae9eedec446efad33e7617c290e24a48e2
Paige Lee <paigelee@Paiges-MacBook-2.local>
2021-12-29 22:43:41 -0500

    initial updates

---
+++ scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var components = ['header','homeleft','homeright','footer']
+    components.forEach(function(comp) {
+        $("#"+comp).load(comp+".html")
+    })
+    $(".content").fadeIn('1000');
+  });
