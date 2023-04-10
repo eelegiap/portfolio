02d0112e2422aed9557e9a8509335097f97927e9
Paige Lee <paigelee@Paiges-MacBook-Air.local>
2023-04-09 22:49:19 -0400

    apr 9 updates

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var components = ['header','homeleft','homeright','footer']
     components.forEach(function(comp) {
         $("#"+comp).load(comp+".html")
+        console.log(comp)
     })
     $(".content").fadeIn('1000');
   });
